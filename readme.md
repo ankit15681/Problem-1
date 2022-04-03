1057a044db50be6dd705853b4deeea1b743b57d0
Ankit Meena <ankit15681@gmail.com>
2022-04-04 00:55:00 +0530

    Final commit

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Initial setup
+After extraction of zip file. Go to main directory or root for respective problem.
+## Installation
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install the packages mentioned in the requirements.txt.
+
+## Run
+
+After installtion of required packages. Run the following command.
+
+```python
+flask run
+# then open your web browser and go to the url.
+https://localhost:5000
--- static/js/index.js
+++ static/js/index.js
@@ -1,30 +1,35 @@
+// element for cards container
 const container = document.getElementById("container");
+//array of strings to populate 2nd column
 const cardTextArray = [
   "He's not the sharpest knife in the drawer",
   "The big building was blazing with the lights",
   "Now you must answer some big questions",
   "Get your act together",
 ];
-
+//maps or find the element id for text boxed using id of btns and inputs
 const parseElementData = (id) => {
   const idx = id.split("-").pop();
-  const name = id.split("-");
+  let name = id.split("-");
+  console.log(name)
+  name = name[0];
   const el = document.getElementById(`textCard-${idx}`);
   return { name, el };
 };
-
+//handles button click 
 const handleClick = (e) => {
   const { name, el } = parseElementData(e.target.id);
-  if (name[0] === "btnBold") el.style.fontWeight = "bold";
-  if (name[0] === "btnItalic") el.style.fontStyle = "italic";
-  if (name[0] === "btnUnderline") el.style.textDecorationLine = "underline";
+  if (name === "btnBold") el.style.fontWeight = "bold";
+  if (name === "btnItalic") el.style.fontStyle = "italic";
+  if (name=== "btnUnderline") el.style.textDecorationLine = "underline";
 };
+//handles input change for color and fontsize
 const handleChange = (e) => {
   const { name, el } = parseElementData(e.target.id);
-  if (name[0] === "inputColor") el.style.color = `${e.target.value}`;
-  if (name[0] === "inputFontSize") el.style.fontSize = `${e.target.value}px`;
+  if (name=== "inputColor") el.style.color = `${e.target.value}`;
+  if (name === "inputFontSize") el.style.fontSize = `${e.target.value}px`;
 };
-
+//html for cards of first column of btns and inputs
 const btnCard = (idx) => {
   return `<div class="btnCard" id="btnCard-${idx}">
         <button id="btnBold-${idx}" value=1 class="btn btn-link" data-toggle="collapse" data-target="#textCard-${idx}" aria-expanded="true" aria-controls="collapse-${idx}">
@@ -37,7 +42,7 @@ const btnCard = (idx) => {
             Underline
         </button>
         <input id="inputColor-${idx}"  type="color" placeholder="Change Color" />
-        <input id="inputFontSize-${idx}"  type="number" placeholder="Enter size font size" />
+        <input id="inputFontSize-${idx}"  type="number" placeholder="Enter font size" />
     </div>`;
 };
 cardTextArray.forEach((text, idx) => {
@@ -51,13 +56,13 @@ cardTextArray.forEach((text, idx) => {
     <div class="card">
     ${btnCard(idx)}
     <div class="textCard" id="textCard-${idx}" class="collapse show" aria-labelledby="btnCard-${idx}" data-parent="#container">
-        ${text}
+       <p>${text}</p>
     </div>
   </div>
   `;
   container.innerHTML += content;
 });
-
+//adds event listeners to the buttons and inputs
 cardTextArray.forEach((text, idx) => {
   ["btnBold", "btnItalic", "btnUnderline"].forEach((item)=>{
         const el = document.getElementById(`${item}-${idx}`);
